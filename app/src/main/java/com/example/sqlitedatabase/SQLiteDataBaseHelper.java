29a1f567b3f7f2f8d8a85c9d9c07fc785514f3e8
jtassin <jean_francois_tassin@hotmail.com>
2019-05-14 17:40:47 +0200

    Retour

--- app/src/main/java/com/example/sqlitedatabase/SQLiteDataBaseHelper.java
+++ app/src/main/java/com/example/sqlitedatabase/SQLiteDataBaseHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class SQLiteDataBaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "book.db";
@@ -23,6 +24,9 @@ public class SQLiteDataBaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(SQLiteDataBaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
